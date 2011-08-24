53c11eef9c4143bb89292bf346e46acbbf48a406
Katharine <kgillis@levementum.com>
2011-08-23 17:58:13 -0700

    Added Chrome integration tests for the Home page.

---
+++ TaskTrackerIntegrationTests/Chrome/HomeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TaskTrackerIntegrationTests.Chrome
+{
+    [TestFixture]
+    [Category("Chrome")]
+    class HomeTests : TaskTrackerIntegrationTests.HomeTests
+    {
+        IWebDriver driver;
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void DefaultURL_RoutesToHomeIndex()
+        {
+            driver = new ChromeDriver();
+
+            base.DefaultURL_RoutesToHomeIndex(driver);
+        }
+    }
+}
