4ff75424201bb3e62e3ceba56c0166c0bf328f0d
Katharine <kgillis@levementum.com>
2011-08-30 14:08:44 -0700

    Got the database to create correctly.

---
+++ TaskTracker/DB/ApplicationServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Management;
+
+namespace TaskTracker.DB
+{
+    public static class ApplicationServices
+    {
+        readonly static string connectionString = WebConfigurationManager.ConnectionStrings["TaskTrackerContext"].ConnectionString;
+        readonly static SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder(connectionString);
+
+        public static void InstallServices(SqlFeatures sqlFeatures)
+        {
+            SqlServices.Install(myBuilder.InitialCatalog, sqlFeatures, connectionString);
+        }
+
+        public static void UninstallServices(SqlFeatures sqlFeatures)
+        {
+            SqlServices.Uninstall(myBuilder.InitialCatalog, sqlFeatures, connectionString);
+        }
+
+        public static string GetInitialCatalog()
+        {
+            return myBuilder.InitialCatalog;
+        }
+    }
+}
---
+++ TaskTracker/DB/TaskTrackerDropCreateDatabaseIfModelChanges.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Profile;
+using System.Web.Security;
+
+namespace TaskTracker.DB
+{
+    public class TaskTrackerDropCreateDatabaseIfModelChanges : DropCreateDatabaseIfModelChanges<TaskTrackerContext>
+    {
+        protected override void Seed(TaskTrackerContext context)
+        {
+            // Set up the membership, roles, and profile systems.
+            ApplicationServices.InstallServices(SqlFeatures.Membership | SqlFeatures.Profile | SqlFeatures.RoleManager);
+
+            // Create the default accounts and roles.
+            if (ApplicationServices.GetInitialCatalog() == "tasktracker_testing")
+            {
+                if (Membership.GetUser("testuser", false) == null)
+                {
+                    Membership.CreateUser("testuser", "password", "testuser@test.com");
+                    MembershipUser user = Membership.GetUser("testuser", false);
+                    user.IsApproved = true;
+
+                    var profile = ProfileBase.Create("testuser");
+                    profile.SetPropertyValue("FirstName", "test");
+                    profile.SetPropertyValue("LastName", "user");
+                    profile.SetPropertyValue("TimeZone", "US Mountain Standard Time");
+                    profile.Save();
+                }
+            }
+        }
+    }
+}
