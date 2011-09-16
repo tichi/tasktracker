27cad0da9a62c89b5bfe6b315db8bc6cef7b15df
Katie Gillis <gillis.katharine@gmail.com>
2011-09-16 08:14:11 -0700

    Issue #2: Started writing integration tests for user detail view.

---
+++ TaskTrackerIntegrationTests/Base/UserDetailViewTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenQA.Selenium;
+
+using NUnit.Framework;
+
+namespace TaskTrackerIntegrationTests.Base
+{
+    abstract class UserDetailViewTests : BasicTestFixture
+    {
+        protected void NonAdministratorUser_CanNavigateToOwnUserDetailView()
+        {
+        }
+    }
+}
